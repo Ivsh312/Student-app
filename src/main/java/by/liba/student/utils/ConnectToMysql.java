94bc860028a5dd7250a8d0eafe61d53479e985a5
Ivsh312 <43249682+Ivsh312@users.noreply.github.com>
2018-11-15 21:26:25 +0300

    Update ConnectToMysql.java

--- src/main/java/by/liba/student/utils/ConnectToMysql.java
+++ src/main/java/by/liba/student/utils/ConnectToMysql.java
@@ -11,7 +11,7 @@ public class ConnectToMysql {
 		Class.forName("com.mysql.jdbc.Driver");
 		con = DriverManager.getConnection(
         		"jdbc:mysql://localhost:3306/BEGANSS", "root",
-				"779259vrun13");
+				"123");
 		return con;
 	}
 }
